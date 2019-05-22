9c15930c6f4adcd4c4b658b3cb87179f6b6ae4b0
SunYuaa <2861164631@qq.com>
2019-05-22 11:46:45 +0000

    bug

---
+++ app/Http/Controllers/Work/SignController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Work;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Redis;
+
+class SignController extends Controller
+{
+    //redis bitmap签到
+    public function sign(){
+
+    }
+}
---
+++ app/Http/Controllers/Work/WxController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Work;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class WxController extends Controller
+{
+    //wx
+    public function
+}
